a25e08c18a2e2187c17bd68e36a26dee20dbd404
T.saikumar <tallurisai9346@gmail.com>
2026-09-04 10:51:50 +0530

    Update counter.js

--- src/counter.js
+++ src/counter.js
@@ -1,9 +1,20 @@
+// export function setupCounter(element) {
+//   let counter = 0
+//   const setCounter = (count) => {
+//     counter = count
+//     element.innerHTML = `Count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
+// }
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `Count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  let count = 0;
+
+  element.textContent = "Count is 0";
+
+  element.addEventListener("click", function () {
+    count = count + 1;
+
+    element.textContent = "Count is " + count;
+  });
 }
